81d7a1af2bf2e6f9adaaa053b9a52fc58f69348c
Jordan Samson <44793715+smuthbudda@users.noreply.github.com>
2023-07-08 17:11:01 +1000

    initial commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { page } from "$app/stores";
+	import logo from "$lib/images/svelte-logo.svg";
+	import github from "$lib/images/github.svg";
+</script>
+
+<header>
+	<nav>
+		<div aria-current={$page.url.pathname === "/" ? "page" : undefined}>
+			<a href="/">Home</a>
+		</div>
+		<div
+			aria-current={$page.url.pathname === "/about" ? "page" : undefined}
+		>
+			<a href="/about">About</a>
+		</div>
+		<div
+			aria-current={$page.url.pathname.startsWith("/sverdle")
+				? "page"
+				: undefined}
+		>
+			<a href="/sverdle">Sverdle</a>
+		</div>
+	</nav>
+</header>
+
+<style>
+</style>
